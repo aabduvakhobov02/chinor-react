76e3f0802fa5dabe14d7cf4c00e6bceff83db1b6
aabduvakhobov <aabduvakhobov02@gmail.com>
2022-01-05 23:48:37 +0500

    Birnima

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,21 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Gallery from "./Gallery";
 
 import Home from "./Home";
+import News from "./News";
+import Services from "./Services";
 
 const Main = () => (
-  <Switch>
+  <Routes>
     <Route exact path="/" component={Home} />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/" component={Home} />
-  </Switch>
+    <Route exact path="/about" component={About} />
+    <Route exact path="/contact" component={Contact} />
+    <Route exact path="/news" component={News} />
+    <Route exact path="/gallery" component={Gallery} />
+    <Route exact path="/services" component={Services} />
+  </Routes>
 );
 
 export default Main;
