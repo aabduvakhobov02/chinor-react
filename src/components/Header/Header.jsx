4aa2115546fb03c5518bd3356d04ad6c38915009
aabduvakhobov <aabduvakhobov02@gmail.com>
2022-01-06 02:07:54 +0500

    Designed Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,44 @@
+import { NavLink } from "react-router-dom";
 import Preheader from "./Preheader";
+import ButtonComponent from "../shared/ButtonComponent";
+
 import ChinorLogo from "../../assets/logo.png";
 import "./Header.css";
 
 const Header = () => {
+  const headerLinks = [
+    { name: "HOME", link: "/" },
+    { name: "ABOUT US", link: "/about" },
+    { name: "SERVICES", link: "/services" },
+    { name: "GALLERY", link: "/gallery" },
+    { name: "CONTACT", link: "/contact" },
+  ];
+
   return (
-    <div>
+    <>
       <Preheader />
-      <div>
-        <img src={ChinorLogo} className="logo" />
-        <div className="header-content"></div>
+      <div className="Container">
+        <div className="Header">
+          <div>
+            <img src={ChinorLogo} className="logo" />
+          </div>
+          <nav className="Header-content">
+            <ul className="menu__list">
+              {headerLinks.map((link) => (
+                <li className="menu__list-item">
+                  <NavLink className="Header-navlink" to={link.link}>
+                    {link.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div>
+            <ButtonComponent title={"MAKE AN APPOINTMENT"} />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Header/Preheader.jsx
+++ src/components/Header/Preheader.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import UsaIcon from "../../assets/flag-icons/usa.svg";
 import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 
@@ -10,17 +10,19 @@ import "./Preheader.css";
 
 const Preheader = () => {
   return (
-    <div className="Container">
-      <div className="Preheader">
-        <div className="Phone-number">
-          <PhoneIcon />
-          <p>Tel: 555-000-556</p>
-        </div>
-        <div className="Language">
-          <Menu menuButton={<MenuButton>ENG</MenuButton>} transition>
-            <MenuItem>RUS</MenuItem>
-            <MenuItem>UZB</MenuItem>
-          </Menu>
+    <div className="Preheader">
+      <div className="Container">
+        <div className="Preheader-wrapper">
+          <div className="Phone-number">
+            <PhoneIcon />
+            <p>Tel: 555-000-556</p>
+          </div>
+          <div className="Language">
+            <Menu menuButton={<MenuButton>ENG</MenuButton>} transition>
+              <MenuItem>RUS</MenuItem>
+              <MenuItem>UZB</MenuItem>
+            </Menu>
+          </div>
         </div>
       </div>
     </div>
